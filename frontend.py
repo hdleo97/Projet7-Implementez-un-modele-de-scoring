70b82c193ced590b56710dfef44ef60be71cbc4c
HLeonel <82946019+hdleo97@users.noreply.github.com>
2024-01-20 15:29:44 +0100

    Add files via upload
    update with fastapi

--- frontend.py
+++ frontend.py
@@ -9,23 +9,39 @@ import shap
 import joblib
 import plotly.express as px
 import imblearn
-
+#from tensorflow.keras.models import load_model
+#Librairie de l'API
+import requests
+import json
+from sklearn.linear_model import LogisticRegression
 
 def main():
+    api_main_url = 'http://localhost:8000'
+    # response = requests.get(api_main_url + '/')
+    # welcome = response.json()
+    # st.write(welcome['welcome'])
+
+    #Chargement du model
+    clf = joblib.load("pipeline_depense_pred.joblib")
+    model = LogisticRegression()
 
     data = pd.read_csv('application_train.csv', encoding ='utf-8')
-    data_test = pd.read_csv('application_test.csv', encoding ='utf-8')
+    df = pd.read_csv('data_pred.csv', encoding ='utf-8')
     target = data['TARGET']
     # data_pred = data.join(data_test)
     # data_pred = data_pred.drop('TARGET')
 
+
+    # Définition des caractéristiques pertinentes (isolement des features non utilisées)
+    ignore_features = ['Unnamed: 0', 'SK_ID_CURR','INDEX', 'TARGET']
+    relevant_features = [col for col in data.columns if col not in ignore_features]
+
     ########Fonction######
 
     def load_model():
-        model = joblib.load("mlflow_model/model.pkl")
-        return clf
+        model = joblib.load("pipeline_depense_pred.joblib") #mlflow_model/model.pkl
+        return model
 
-    @st.cache
     def load_infos_gen(data):
         lst_infos = [data.shape[0],
                      round(data["AMT_INCOME_TOTAL"].mean(), 2),
@@ -42,27 +58,89 @@ def main():
         data_client = data[data['SK_ID_CURR'] == int(id)]
         return data_client
 
-    @st.cache
     def load_age_population(data):
         data_age = round((data["DAYS_BIRTH"]/365), 2)
         return data_age
 
-    @st.cache
     def load_income_population(sample):
         df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
         df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
         return df_income
 
-    @st.cache
-    def load_prediction(sample, id, clf):
-        X=sample.iloc[:, :-1]
-        score = clf.predict_proba(X[X.index == int(id)])[:,1]
-        return score
+    def load_prediction(id):
+        api_url = api_main_url + f"/credit/{id}"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return []
 
-    #Chargement du model
-    id_client = data['SK_ID_CURR']
-    clf = load_model()
+    def get_client_ids():
+        api_url = api_main_url + "/client_ids"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return []
 
+    # Fonction pour récupérer les valeurs SHAP pour un client spécifique
+    def get_shap_values_by_client(client_id):
+        api_url = api_main_url + "/shap_values/"
+        response = requests.get(f"{api_url}{client_id}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            st.error(f"Error fetching SHAP values. Status Code: {response.status_code}")
+            return None
+
+    # Fonction pour afficher le graphique de feature importance
+    def plot_feature_importance(features, values):
+        fig = px.bar(x=features, y=values, labels={'x': 'Feature', 'y': 'SHAP Value'},
+                     title='Feature Importance - SHAP Values')
+        st.plotly_chart(fig)
+
+    # Définition de la route pour récupérer les valeurs SHAP par client
+    def get_shap_values_by_client(client_id):
+        # Filtre les données du client en question
+        client_data = data[data['SK_ID_CURR'] == client_id]
+        client_data = client_data[relevant_features]
+
+        # Créer l'explainer avec le masker
+        # masker = shap.maskers.Independent(data=clf.named_steps['preprocessor'].transform(X), max_samples=100)
+        # explainer = shap.LinearExplainer(clf.named_steps['classifier'], masker)
+
+        # Extraire le modèle réel du pipeline
+        #model = clf.named_steps['model']  # Remplacez 'classifier' par la clé appropriée
+        explainer = shap.LinearExplainer(model, client_data)
+        #explainer = shap.LinearExplainer(clf)  # Utilisation de TreeExplainer au lieu de LinearExplainer
+
+        # Calcul des valeurs SHAP pour le client
+        shap_values = explainer.shap_values(client_data)
+
+        # Conversion des valeurs SHAP en un objet JSON
+        shap_values_json = {
+            "features": relevant_features,
+            "values": shap_values[0].tolist()
+        }
+        return shap_values_json
+
+    def get_nearest_neighbors(client_id):
+        api_url = api_main_url + f"/nearest_neighbors/{client_id}"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return []
+
+    def get_datadrift_plot():
+        api_url = api_main_url + "/data_drift_html"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            return response
+        else:
+            return []
+
+    id_client = get_client_ids()
     ####HEADER#####
     # st.image("Logo.png")
 
@@ -110,41 +188,41 @@ def main():
     # HOME PAGE - MAIN CONTENT
     #######################################
     #Display Customer ID from Sidebar
-    st.write("Customer ID selection :", chk_id)
+    st.write("Identifiant du client sélectionné:", chk_id)
 
 
     #Customer information display : Customer Gender, Age, Family status, Children, …
-    st.header("**Affichage des information client**")
+    st.header("**Affichage des informations client**")
 
-    if st.checkbox("Show customer information ?"):
+    if st.checkbox("Afficher les informations ?"):
 
         infos_client = identite_client(data, chk_id)
-        st.write("**Gender : **", infos_client["CODE_GENDER"].values[0])
+        st.write("**Genre : **", infos_client["CODE_GENDER"].values[0])
         st.write("**Age : **{:.0f} ans".format(int(infos_client["DAYS_BIRTH"]/365)))
-        st.write("**Family status : **", infos_client["NAME_FAMILY_STATUS"].values[0])
-        st.write("**Number of children : **{:.0f}".format(infos_client["CNT_CHILDREN"].values[0]))
+        st.write("**Status familiale : **", infos_client["NAME_FAMILY_STATUS"].values[0])
+        st.write("**Nombre d'enfants : **{:.0f}".format(infos_client["CNT_CHILDREN"].values[0]))
 
         #Age distribution plot
         data_age = load_age_population(data)
         fig, ax = plt.subplots(figsize=(10, 5))
         sns.histplot(data_age, edgecolor = 'k', color="goldenrod", bins=20)
         ax.axvline(int(infos_client["DAYS_BIRTH"].values / 365), color="green", linestyle='--')
-        ax.set(title='Customer age', xlabel='Age(Year)', ylabel='')
+        ax.set(title='Âge client', xlabel='Age(Year)', ylabel='')
         st.pyplot(fig)
 
 
-        st.subheader("*Income (USD)*")
-        st.write("**Income total : **{:.0f}".format(infos_client["AMT_INCOME_TOTAL"].values[0]))
-        st.write("**Credit amount : **{:.0f}".format(infos_client["AMT_CREDIT"].values[0]))
-        st.write("**Credit annuities : **{:.0f}".format(infos_client["AMT_ANNUITY"].values[0]))
-        st.write("**Amount of property for credit : **{:.0f}".format(infos_client["AMT_GOODS_PRICE"].values[0]))
+        st.subheader("*Revenu (USD)*")
+        st.write("**Revenu total : **{:.0f}".format(infos_client["AMT_INCOME_TOTAL"].values[0]))
+        st.write("**Montant de crédit : **{:.0f}".format(infos_client["AMT_CREDIT"].values[0]))
+        st.write("**Rentes de crédit : **{:.0f}".format(infos_client["AMT_ANNUITY"].values[0]))
+        st.write("**Montant de crédit : **{:.0f}".format(infos_client["AMT_CREDIT"].values[0]))
 
         #Income distribution plot
         data_income = load_income_population(data)
         fig, ax = plt.subplots(figsize=(10, 5))
         sns.histplot(data_income["AMT_INCOME_TOTAL"], edgecolor = 'k', color="goldenrod", bins=10)
         ax.axvline(int(infos_client["AMT_INCOME_TOTAL"].values[0]), color="green", linestyle='--')
-        ax.set(title='Customer income', xlabel='Income (USD)', ylabel='')
+        ax.set(title='Revenu client', xlabel='Income (USD)', ylabel='')
         st.pyplot(fig)
 
         #Relationship Age / Income Total interactive plot
@@ -173,16 +251,19 @@ def main():
 
     #Customer solvability display
     st.header("**Customer file analysis**")
-    prediction = load_prediction(data, chk_id, clf)
-    st.write("**Default probability : **{:.0f} %".format(round(float(prediction)*100, 2)))
+    prediction = load_prediction(chk_id)
+    st.write("**Prédiction** : ", prediction['Prédiction'])
+    st.write("Probabilité de réalisation: {:.0f} %".format(round(float(prediction['Probabilité'])*100, 2)))
+    st.write("Score du client :", round((prediction['Probabilité'])*100,2))
+    st.write("Interpretation: ",format(prediction['Conclusion']))
 
     #Compute decision according to the best threshold
-    #if prediction <= xx :
-    #    decision = "<font color='green'>**PRÊT ACCORDE**</font>"
-    #else:
-    #    decision = "<font color='red'>**PRÊT REJETE**</font>"
+    if prediction['Prédiction'] == 0 :
+       decision = "<font color='green'>**PRÊT ACCEPTE**</font>"
+    else:
+       decision = "<font color='red'>**PRÊT REJETE**</font>"
 
-    #st.write("**Decision** *(with threshold xx%)* **: **", decision, unsafe_allow_html=True)
+    st.write("**Decision** *(with threshold xx%)* **: **", decision, unsafe_allow_html=True)
 
     st.markdown("<u>Customer Data :</u>", unsafe_allow_html=True)
     st.write(identite_client(data, chk_id))
@@ -190,41 +271,36 @@ def main():
 
     #Feature importance / description
     if st.checkbox("Customer ID {:.0f} feature importance ?".format(chk_id)):
-        shap.initjs()
-        X = sample.iloc[:, :-1]
-        X = X[X.index == chk_id]
-        number = st.slider("Pick a number of features…", 0, 20, 5)
 
-        fig, ax = plt.subplots(figsize=(10, 10))
-        explainer = shap.LinearExplainer(load_model())
-        shap_values = explainer.shap_values(X)
-        shap.summary_plot(shap_values[0], X, plot_type ="bar", max_display=number, color_bar=False, plot_size=(5, 5))
-        st.pyplot(fig)
+        st.header("Feature importance globale")
+        st.image("feature_importance_globale.png")
+        # Récupération des valeurs SHAP pour le client spécifié
+        # shap_data = get_shap_values_by_client(chk_id)
 
-        if st.checkbox("Need help about feature description ?") :
-            list_features = description.index.to_list()
-            feature = st.selectbox('Feature checklist…', list_features)
-            st.table(description.loc[description.index == feature][:1])
-
-    else:
-        st.markdown("<i>…</i>", unsafe_allow_html=True)
-
+        # Affichage du graphique de feature importance si les valeurs SHAP sont disponibles
+        # if shap_data:
+        #     features = shap_data.get("features", [])
+        #     values = shap_data.get("values", [])
+        #     st.header("Features importance locale")
+        #     plot_feature_importance(features, values)
+        #
+        # else:
+        #     st.markdown("<i>…</i>", unsafe_allow_html=True)
 
 
     #Similar customer files display
     chk_voisins = st.checkbox("Show similar customer files ?")
 
     if chk_voisins:
-        knn = load_knn(sample)
+        knn_data = get_nearest_neighbors(chk_id)
         st.markdown("<u>List of the 10 files closest to this Customer :</u>", unsafe_allow_html=True)
-        st.dataframe(load_kmeans(sample, chk_id, knn))
+        st.dataframe(knn_data)
         st.markdown("<i>Target 1 = Customer with default</i>", unsafe_allow_html=True)
     else:
         st.markdown("<i>…</i>", unsafe_allow_html=True)
 
-
     st.markdown('***')
-    st.markdown("Thanks for going through this Web App with me! I'd love feedback on this, so if you want to reach out you can find me on [twitter] (https://twitter.com/nalron_) or my [website](https://nalron.com/). *Code from [Github](https://github.com/nalron/project_credit_scoring_model)* ❤️")
+    st.markdown("Thanks ❤️")
 
 
 if __name__ == '__main__':
